3907d5e9a86166ff23d16726d0a0b6be698158c0
Sepideh Dana <sep.dana28@gmail.com>
2022-08-17 16:05:52 +0100

    added missing file

---
+++ myblog/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace myblog.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+        }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string htmlContent { get; set; }
+
+    }
+
+}
+
