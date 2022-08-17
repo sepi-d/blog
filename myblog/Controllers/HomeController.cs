754c74df13197cb40f339d3f6802d2f118c66c23
Sepideh Dana <sep.dana28@gmail.com>
2022-08-17 15:57:42 +0100

    read Json file and display

---
+++ myblog/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using myblog.Models;
+using Newtonsoft.Json;
+
+namespace myblog.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        //todo add try anf catch
+        // read Json file 
+        var BlogJson = System.IO.File.ReadAllText("App_Data/Blog-Posts.json");
+        // convert Json to a generic array 
+        var jsonobject = JsonConvert.DeserializeObject(BlogJson);
+
+        // send array to view 
+
+        ViewBag.Posts = jsonobject;
+
+        return View();
+    }
+
+  
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ myblog/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace myblog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    //public int id { get; set; }
+    //public string title { get; set; }
+}
+
